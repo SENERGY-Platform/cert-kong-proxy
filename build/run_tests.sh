f7e3b258cdb45a2f6cf40e2af4efd39413f48357
Hannes Hansen <mail@hannesh.de>
2022-11-21 12:12:30 +0100

    changed test volume

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -2,5 +2,5 @@ coverage run -m unittest discover tests
 coverage xml -o coverage.xml --omit="*/test*"
 coverage report --omit="*/test*"
 coverage html --omit="*/test*"
-mv coverage.xml /tests/coverage.xml
-mv htmlcov /tests/htmlcov
+mv coverage.xml /coverage/coverage.xml
+mv htmlcov /coverage/htmlcov
