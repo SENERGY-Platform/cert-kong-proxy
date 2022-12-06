66433da17205a2dde9e93bb1d440794ba59711f2
Hannes Hansen <mail@hannesh.de>
2022-12-06 11:03:25 +0100

    debug tests

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,6 +1,8 @@
 coverage run -m unittest discover tests
+TEST_RESULT=$?
 coverage xml -o coverage.xml --omit="*/test*"
 coverage report --omit="*/test*"
 coverage html --omit="*/test*"
 mv coverage.xml /coverage/coverage.xml
 mv htmlcov /coverage/htmlcov
+exit $TEST_RESULT
